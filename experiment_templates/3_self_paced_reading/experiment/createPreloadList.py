904fdd874e6491b1bec9ea5c40438989f228e21f
thegricean <judithdegen@gmail.com>
2017-04-20 10:10:11 -0700

    moved exps

--- experiment_templates/3_self_paced_reading/experiment/createPreloadList.py
+++
@@ -1,4 +0,0 @@
-import os
-
-
-print("\",\"images/".join(os.listdir("/Users/titlis/cogsci/projects/xprag.de/self_paced_reading/spr_pilot/experiment/images")))
--- experiment_templates/4_dragdrop_production/experiment/createPreloadList.py
+++
@@ -1,4 +0,0 @@
-import os
-
-
-print("\",\"pictures/".join(os.listdir("/Users/elisakreiss/Documents/stanford/study/overinformativeness/experiments/19_18_enhanced/experiment/pictures")))
