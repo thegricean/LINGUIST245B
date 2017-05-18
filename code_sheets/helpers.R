db37e53d11935998ca5d3a37cc909cca6bd99598
thegricean <judithdegen@gmail.com>
2017-05-18 09:49:04 -0700

    added ggplot code sheet

---
+++ code_sheets/helpers.R
@@ -0,0 +1,46 @@
+
+myCenter <- function(x) {
+  if (is.numeric(x)) { return(x - mean(x)) }
+  if (is.factor(x)) {
+    x <- as.numeric(x)
+    return(x - mean(x))
+  }
+  if (is.data.frame(x) || is.matrix(x)) {
+    m <- matrix(nrow=nrow(x), ncol=ncol(x))
+    colnames(m) <- paste("c", colnames(x), sep="")
+    for (i in 1:ncol(x)) {
+      if (is.factor(x[,i])) {
+        y <- as.numeric(x[,i])
+        m[,i] <- y - mean(y, na.rm=T)
+      }
+      if (is.numeric(x[,i])) {
+        m[,i] <- x[,i] - mean(x[,i], na.rm=T)
+      }
+    }
+    return(as.data.frame(m))
+  }
+}
+
+se <- function(x)
+{
+  y <- x[!is.na(x)] # remove the missing values, if any
+  sqrt(var(as.vector(y))/length(y))
+}
+
+zscore <- function(x){
+  ## Returns z-scored values
+  x.mean <- mean(x)
+  x.sd <- sd(x)
+  
+  x.z <- (x-x.mean)/x.sd
+  
+  return(x.z)
+}
+
+## for bootstrapping 95% confidence intervals
+library(bootstrap)
+theta <- function(x,xdata,na.rm=T) {mean(xdata[x],na.rm=na.rm)}
+ci.low <- function(x,na.rm=T) {
+  mean(x,na.rm=na.rm) - quantile(bootstrap(1:length(x),1000,theta,x,na.rm=na.rm)$thetastar,.025,na.rm=na.rm)}
+ci.high <- function(x,na.rm=T) {
+  quantile(bootstrap(1:length(x),1000,theta,x,na.rm=na.rm)$thetastar,.975,na.rm=na.rm) - mean(x,na.rm=na.rm)}
