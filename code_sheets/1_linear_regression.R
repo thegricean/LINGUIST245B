a4b4715c6b0597d788093a49ce52962fe9b76620
thegricean <judithdegen@gmail.com>
2023-05-15 21:56:44 -0700

    updated code sheets

--- code_sheets/1_linear_regression.R
+++ code_sheets/1_linear_regression.R
@@ -1,10 +1,11 @@
 # R basics and linear regression
 # Created by jdegen on Sep 16, 2016
-# Updated by jdegen on May 7, 2020
+# Updated by jdegen on May 15, 2023
 
-# Load the `languageR` package. If it's not yet installed you'll get an error saying "Error in library(languageR) : there is no package called ‘languageR’". To install the package, first type and execute `install.packages("languageR")`. (This generalizes to any package, using the name of the package instead of "languageR".)
+# Load the `languageR` and `brms` packages. If they're not yet installed you'll get an error saying "Error in library(languageR) : there is no package called ‘languageR’". To install the package, first type and execute `install.packages("languageR")`. (This generalizes to any package, using the name of the package instead of "languageR".)
 library(languageR)
-library(ggplot2)
+library(tidyverse)
+library(brms)
 
 # This will also load the lexical decision time dataset from Baayen et al (2006), which we will be modeling extensively. To see two different summaries of the dataset and the first few lines:
 summary(lexdec)
@@ -29,52 +30,83 @@ mean(lexdec$RT)
 min(lexdec$RT)
 sd(lexdec$RT)
 
-# Let's run our first linear regression model! We start by asking whether log frequency has a linear effect on log RTs:
-m = lm(RT ~ Frequency, data=lexdec)
-summary(m)
+# Let's recode the language background variable from "NativeLanguage" to "LanguageBackground", and its levels from "English" & "Other" to "English" & "Non-English"
+lexdec = lexdec %>% 
+  rename("LanguageBackground"="NativeLanguage") %>% 
+  mutate(LanguageBackground = fct_recode(LanguageBackground, "Non-English"="Other"))
 
-# To get a sense for whether the assumptions of homoscedasticity and normality of residuals are met, let's run a model with untransformed RTs and frequencies to contrast with:
+# 4. How many data points are in the English and Non-English groups?
+table(lexdec$LanguageBackground)
+
+
+# Frequentist stats
+# Let's run our first linear regression model! We start by asking whether language background has a linear effect on log RTs:
+m.f = lm(RT ~ LanguageBackground, data=lexdec)
+summary(m.f)
+
+# Bayesian stats
+# Let's run the exact same model the Bayesian way! We haven't specified a prior, so the model will assume weakly informative default priors.
+m.b = brm(RT ~ LanguageBackground, data=lexdec)
+summary(m.b)
+
+# 5. Extend the simple model to include an additional predictor for frequency. If you don't remember the name of the frequency column in the dataset, use the names() function.
+
+# Frequentist: 
+m.f = lm(RT ~ LanguageBackground + Frequency, data=lexdec)
+summary(m.f)
+
+# Bayesian:
+m.b = brm(RT ~ LanguageBackground + Frequency, data=lexdec)
+summary(m.b)
+
+
+# 6. Let's extend the model to include the interaction between frequency and language background. In order to interpret interaction terms, we need to know how predictors are coded. By default, R dummy-codes categorical predictors. It assigns 0 and 1 to the predictors in alphabetical order. If you're not sure how a predictor is coded (or if you want to change the default coding), you can use the contrasts() function. What is the reference level for the LanguageBackground predictor?
+
+contrasts(lexdec$LanguageBackground)
+
+# These are two equivalent ways of adding the interaction between frequency and language background to the model:
+
+# Frequentist: 
+m.f = lm(RT ~ Frequency + LanguageBackground + Frequency:LanguageBackground, data=lexdec)
+summary(m.f)
+m.f = lm(RT ~ Frequency*LanguageBackground, data=lexdec)
+summary(m.f)
+
+# Bayesian: 
+m.b = brm(RT ~ Frequency + LanguageBackground + Frequency:LanguageBackground, data=lexdec)
+summary(m.b)
+m.b = brm(RT ~ Frequency*LanguageBackground, data=lexdec)
+summary(m.b)
+
+
+# Extra: To get a sense for whether the linear modeling assumptions of homoscedasticity and normality of residuals are met, let's run a model with untransformed RTs and frequencies to contrast with:
 lexdec$rawRT = exp(lexdec$RT)
 lexdec$rawFrequency = exp(lexdec$Frequency)
 
 m.raw = lm(rawRT ~ rawFrequency, data = lexdec)
 summary(m.raw)
 
-# create histograms of residualsto apply the interocular test for normality
+# Create histograms of residuals to apply the "interocular" test for normality (i.e., look at the distributions).
 lexdec$residual = residuals(m)
 lexdec$rawResidual = residuals(m.raw)
 
-# normality of residuals assumption met (reasonably well):
+# Normality of residuals assumption met (reasonably well) for log RTs:
 ggplot(lexdec, aes(x=residual)) +
   geom_histogram()
 
-# normality of residuals assumption not met:
+# Normality of residuals assumption not met for raw RTs:
 ggplot(lexdec, aes(x=rawResidual)) +
   geom_histogram()
 
-# create plots of residuals against frequency to apply interocular test for homoscedasticity
+# Create plots of residuals against frequency to apply interocular test for homoscedasticity.
 
-# homoscedasticity assumption met:
+# Homoscedasticity assumption met for log RTs:
 ggplot(lexdec, aes(x=Frequency,y=residual)) +
   geom_point()
 
-# homoscedasticity assumption NOT met:
+# Homoscedasticity assumption NOT met for raw RTs:
 ggplot(lexdec, aes(x=rawFrequency,y=rawResidual)) +
   geom_point()
 
 
-# 5. Extend the simple model to include an additional predictor for morphological family size. If you don't remember the name of the family size column in the model, use the `names` command.
-m = lm(RT ~ Frequency + FamilySize, data=lexdec)
-summary(m)
 
-# 6. Extend the model to include a predictor for participants’ native language (English vs other). By default R dummy-codes categorical predictors. It assigns 0 and 1 to the predictors in alphabetical order. If you're not sure how a predictor is coded (or if you want to change the default coding), you can use the `contrasts()` function. What is the reference level for the NativeLanguage predictor?
-m = lm(RT ~ Frequency + FamilySize + NativeLanguage, data=lexdec)
-summary(m)
-
-contrasts(lexdec$NativeLanguage)
-
-# Let's extend the model to include the interaction between frequency and native language. These are two equivalent ways of doing that:
-m = lm(RT ~ Frequency + NativeLanguage + Frequency:NativeLanguage, data=lexdec)
-summary(m)
-m = lm(RT ~ Frequency*NativeLanguage, data=lexdec)
-summary(m)
--- code_sheets/1_linear_regression_withprompts.R
+++ code_sheets/1_linear_regression_withprompts.R
@@ -1,9 +1,11 @@
 # R basics and linear regression
 # Created by jdegen on Sep 16, 2016
-# Updated by jdegen on Apr 25, 2018
+# Updated by jdegen on May 15, 2023
 
-# Load the `languageR` package. If it's not yet installed you'll get an error saying "Error in library(languageR) : there is no package called ‘languageR’". To install the package, first type and execute `install.packages("languageR")`. (This generalizes to any package, using the name of the package instead of "languageR".)
+# Load the `languageR` and `brms` packages. If they're not yet installed you'll get an error saying "Error in library(languageR) : there is no package called ‘languageR’". To install the package, first type and execute `install.packages("languageR")`. (This generalizes to any package, using the name of the package instead of "languageR".)
 library(languageR)
+library(tidyverse)
+library(brms)
 
 # This will also load the lexical decision time dataset from Baayen et al (2006), which we will be modeling extensively. To see two different summaries of the dataset and the first few lines:
 summary(lexdec)
@@ -16,11 +18,11 @@ View(lexdec)
 
 # We are interested in modeling response times (coded in the data frame as 'RT'). The first step is always to understand some basic things about your data.
 
-# 1. How many data points are there? 
+# 1. How many data points are there?
 # Hint: use the nrow() function.
 
 
-# 2. How many unique participants are there? 
+# 2. How many unique participants are there?
 # Hint: Use the length() and unique() functions.
 
 
@@ -28,22 +30,43 @@ View(lexdec)
 # Hint: Use the mean(), min(), and sd() functions.
 
 
-# Let's run our first linear regression model! We start by asking whether frequency has a linear effect on log RTs:
-m = lm(RT ~ Frequency, data=lexdec)
-summary(m)
+# Let's recode the language background variable from "NativeLanguage" to "LanguageBackground", and its levels from "English" & "Other" to "English" & "Non-English"
+lexdec = lexdec %>% 
+  rename("LanguageBackground"="NativeLanguage") %>% 
+  mutate(LanguageBackground = fct_recode(LanguageBackground, "Non-English"="Other"))
+
+# 4. How many data points are in the English and Non-English groups?
+# Hint: Use the table() function.
 
 
-# 5. Extend the simple model to include an additional predictor for morphological family size. 
-# Hint: Predictors are added to formulae with '+'
-# Hint: If you don't remember the name of the family size column in the model, use the `names` command.
+# Frequentist stats
+# Let's run our first linear regression model! We start by asking whether language background has a linear effect on log RTs:
+m.f = lm(RT ~ LanguageBackground, data=lexdec)
+summary(m.f)
+
+# Bayesian stats
+# Let's run the exact same model the Bayesian way! We haven't specified a prior, so the model will assume weakly informative default priors.
+m.b = brm(RT ~ LanguageBackground, data=lexdec)
+summary(m.b)
+
+# 5. Extend the simple model to include an additional predictor for frequency. If you don't remember the name of the frequency column in the dataset, use the names() function.
 
 
-# 6. Extend the model to include a predictor for participants’ native language (English vs other). By default R dummy-codes categorical predictors. It assigns 0 and 1 to the predictors in alphabetical order. If you're not sure how a predictor is coded (or if you want to change the default coding), you can use the `contrasts()` function. What is the reference level for the NativeLanguage predictor?
 
+# 6. Let's extend the model to include the interaction between frequency and language background. In order to interpret interaction terms, we need to know how predictors are coded. By default, R dummy-codes categorical predictors. It assigns 0 and 1 to the predictors in alphabetical order. If you're not sure how a predictor is coded (or if you want to change the default coding), you can use the contrasts() function. What is the reference level for the LanguageBackground predictor?
 
-# Let's extend the model to include the interaction between frequency and native language. These are two equivalent ways of doing that:
-m = lm(RT ~ Frequency + FamilySize + NativeLanguage + Frequency:NativeLanguage, data=lexdec)
-m = lm(RT ~ FamilySize + Frequency*NativeLanguage, data=lexdec)
-summary(m)
+contrasts(lexdec$LanguageBackground)
 
+# These are two equivalent ways of adding the interaction between frequency and language background to the model:
 
+# Frequentist: 
+m.f = lm(RT ~ Frequency + LanguageBackground + Frequency:LanguageBackground, data=lexdec)
+summary(m.f)
+m.f = lm(RT ~ Frequency*LanguageBackground, data=lexdec)
+summary(m.f)
+
+# Bayesian: 
+m.b = brm(RT ~ Frequency + LanguageBackground + Frequency:LanguageBackground, data=lexdec)
+summary(m.b)
+m.b = brm(RT ~ Frequency*LanguageBackground, data=lexdec)
+summary(m.b)
