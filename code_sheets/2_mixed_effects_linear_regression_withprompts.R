546ae4872c3d6e9c703467fb7d7c1770b9681ba6
thegricean <judithdegen@gmail.com>
2023-05-23 09:40:52 -0700

    code sheet update

--- code_sheets/2_mixed_effects_linear_regression_withprompts.R
+++ code_sheets/2_mixed_effects_linear_regression_withprompts.R
@@ -1,55 +1,98 @@
 # Mixed effects linear regression
 # Created by jdegen on Sep 16, 2016
-# Updated by jdegen on May 7, 2020
+# Updated by jdegen on May 23, 2023
 
 library(languageR)
+library(tidyverse)
 library(lme4)
 library(MuMIn)
+library(brms)
 
-# Let's run our first mixed effects linear regression model! We start by running the simplest linear regression model for predicting RTs (only frequency predictor) with by-subjects random intercepts.
-m = lmer(RT ~ Frequency + (1|Subject), data=lexdec, REML=F)
+theme_set(theme_bw())
+
+# Preliminaries: Let's recode the language background variable and then create a column with centered predictors.
+lexdec = lexdec %>% 
+  rename("LanguageBackground"="NativeLanguage") %>% 
+  mutate(LanguageBackground = fct_recode(LanguageBackground, "Non-English"="Other"))
+
+lexdec = lexdec %>% 
+  mutate(cFrequency = Frequency - mean(Frequency),
+         cLanguageBackground = as.numeric(as.factor(LanguageBackground)) - mean(as.numeric(as.factor(LanguageBackground))))
+
+# Let's run our first mixed effects linear regression model! We start by running the simplest linear regression model for predicting RTs from centered fixed effects of frequency, language background, and their interaction, with by-participant random intercepts.
+m = lmer(RT ~ cFrequency*cLanguageBackground + (1|Subject), data=lexdec, REML=F)
 summary(m)
 
-# marginal R^2 (variance explained by fixed effects): .05
-# conditional R^2 (variance explained by fixed and random effects jointly): .45
+# Let's look more closely at the random effects. What do the numbers represent?
+ranef(m)
+
+# Let's add by-participant random slopes for frequency:
+m = lmer(RT ~ cFrequency*cLanguageBackground + (1+cFrequency|Subject), data=lexdec, REML=F)
+summary(m)
+
+# Let's look more closely at the random effects. What do the numbers represent?
+ranef(m)
+
+# What does the correlation between the random by-participant intercept and slope adjustment mean?
+re = tibble(intercept=ranef(m)$Subject[1][,c("(Intercept)")],slope_freq=ranef(m)$Subject[2][,c("cFrequency")])
+
+ggplot(re, aes(x=intercept,y=slope_freq)) +
+  geom_hline(yintercept=0,linetype="dashed",alpha=.5) +
+  geom_vline(xintercept=0,linetype="dashed",alpha=.5) +
+  geom_smooth(method='lm') +
+  geom_point()
+
+# How much variance is explained by the model?
 r.squaredGLMM(m)
 
-# 1. What was the linear model's R-squared? Re-compute it here.
+# Marginal R^2 (variance explained by fixed effects): .16
+# Conditional R^2 (variance explained by fixed and random effects jointly): .45
 
 
-
-# 2. Let's add random by-subject and by-item intercepts. (What counts as an item?)
-
-
-
-# Let's look more closely at the random effects:
-ranef(m)
-mean(ranef(m)$Subject[[1]])
-head(ranef(m)$Subject[1])
-max(ranef(m)$Subject[1])
-min(ranef(m)$Subject[1])
-
-mean(ranef(m)$Word[[1]])
-head(ranef(m)$Word[1])
-max(ranef(m)$Word[1])
-min(ranef(m)$Word[1])
-
-# Let's add random by-subject slopes for frequency.
-m = lmer(RT ~ Frequency + (1 + Frequency|Subject) + (1|Word), data=lexdec, REML=F)
+# Let's add by-item random effects.
+m = lmer(RT ~ cFrequency*cLanguageBackground + (1+cFrequency|Subject) + (1+cLanguageBackground|Word), data=lexdec, REML=F)
 summary(m)
 
-# To get rid of correlations between random effects:
-m.nocorr = lmer(RT ~ Frequency + (1|Subject) + (0 + Frequency|Subject) + (1|Word), data=lexdec, REML=F)
-summary(m.nocorr)
+ranef(m)
 
-# There are now two adjustment columns by subject, one for the intercepts and one for the slopes. We can see using summary() that there is greater variance in the intercepts than in the slopes. 
-head(ranef(m)$Subject)
-summary(ranef(m)$Subject)
+re_participant = tibble(intercept=ranef(m)$Subject[1][,c("(Intercept)")],slope=ranef(m)$Subject[2][,c("cFrequency")],type="participant")
 
-# What does that correlation of -.92 mean?
-plot(ranef(m)$Subject)
+re_item = tibble(intercept=ranef(m)$Word[1][,c("(Intercept)")],slope=ranef(m)$Word[2][,c("cLanguageBackground")],type="item")
 
-# How do we get p-values? The lmer authors make various suggestions here:
+re = bind_rows(re_participant,re_item)
+
+ggplot(re, aes(x=intercept,y=slope)) +
+  geom_hline(yintercept=0,linetype="dashed",alpha=.5) +
+  geom_vline(xintercept=0,linetype="dashed",alpha=.5) +
+  geom_smooth(method='lm') +
+  geom_point() +
+  facet_wrap(~type)
+
+
+# Let's run the same model the Bayesian way:
+m.b = brm(RT ~ cFrequency*cLanguageBackground + (1+cFrequency|Subject) + (1+cLanguageBackground|Word), 
+          data=lexdec,
+          cores = 4)
+summary(m.b)
+
+# Hypothesis-testing. What is the Bayes Factor and the probability of the frequency main effect being greater than 0?
+h <- hypothesis(m.b, "cFrequency < 0")
+print(h, digits = 4)
+
+# Hypothesis-testing. What is the Bayes Factor and the probability of the language background main effect being greater than 0?
+h <- hypothesis(m.b, "cLanguageBackground > 0")
+print(h, digits = 4)
+
+# Hypothesis-testing. What is the Bayes Factor and the probability of the interaction between frequency and language background being smaller than 0?
+h <- hypothesis(m.b, "cFrequency:cLanguageBackground < 0")
+print(h, digits = 4)
+
+# To compute R^2 (https://github.com/jgabry/bayes_R2/)
+bayes_R2(m.b)
+
+
+
+# How do we get p-values in frequentist models? The lmer authors make various suggestions here:
 ?pvalues
 
 # To compute p-values for fixed effects via model comparison:
